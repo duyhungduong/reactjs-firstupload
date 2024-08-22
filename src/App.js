6e9805ee5fac709e99c039dc2969496511870f2e
duyhungduong <duyhungduong10@gmail.com>
2024-08-22 12:53:10 +0700

    bai 11

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 //import logo from "./logo.svg";
 import "./App.css";
+import { YoutubeData } from "./YoutubeData";
 
 function App() {
+  console.log(YoutubeData);
+
   return (
     <div className="youtube-list">
       <YoutubeItem
@@ -11,19 +14,19 @@ function App() {
         author="DUYHUNG"
       ></YoutubeItem>
       <YoutubeItem
-        image="https://images.unsplash.com/photo-1724093121168-c4ff94115732?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMHx8fGVufDB8fHx8fA%3D%3D"
+        image="https://images.unsplash.com/photo-1721332154373-17e78d19b4a4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMjR8fHxlbnwwfHx8fHw%3D"
         avt="https://images.unsplash.com/photo-1723737347273-5ae32dcdb5d3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzOXx8fGVufDB8fHx8fA%3D%3D"
         info="First  video"
         author="DUYHUNG"
       ></YoutubeItem>
       <YoutubeItem
-        image="https://images.unsplash.com/photo-1724093121168-c4ff94115732?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMHx8fGVufDB8fHx8fA%3D%3D"
+        image="https://images.unsplash.com/photo-1724055420328-184b76676138?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMTh8fHxlbnwwfHx8fHw%3D"
         avt="https://images.unsplash.com/photo-1723737347273-5ae32dcdb5d3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzOXx8fGVufDB8fHx8fA%3D%3D"
         info="First  video"
         author="DUYHUNG"
       ></YoutubeItem>
       <YoutubeItem
-        image="https://images.unsplash.com/photo-1724093121168-c4ff94115732?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMHx8fGVufDB8fHx8fA%3D%3D"
+        image="https://images.unsplash.com/photo-1724008563697-ce1039f74336?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMTd8fHxlbnwwfHx8fHw%3D"
         avt="https://images.unsplash.com/photo-1723737347273-5ae32dcdb5d3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzOXx8fGVufDB8fHx8fA%3D%3D"
         info="First  video"
         author="DUYHUNG"
---
+++ src/YoutubeData.js
@@ -0,0 +1,38 @@
+const YoutubeData = [
+  {
+    id: 1,
+    author: "Example author",
+    title: "Example title",
+    image:
+      "https://images.unsplash.com/photo-1724093121168-c4ff94115732?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMHx8fGVufDB8fHx8fA%3D%3D",
+    avatar:
+      "https://images.unsplash.com/photo-1723737347273-5ae32dcdb5d3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzOXx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    author: "Example author",
+    title: "Example title",
+    image:
+      "https://images.unsplash.com/photo-1724093121168-c4ff94115732?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMHx8fGVufDB8fHx8fA%3D%3D",
+    avatar:
+      "https://images.unsplash.com/photo-1723737347273-5ae32dcdb5d3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzOXx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    author: "Example author",
+    title: "Example title",
+    image:
+      "https://images.unsplash.com/photo-1724093121168-c4ff94115732?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMHx8fGVufDB8fHx8fA%3D%3D",
+    avatar:
+      "https://images.unsplash.com/photo-1723737347273-5ae32dcdb5d3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzOXx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    author: "Example author",
+    title: "Example title",
+    image:
+      "https://images.unsplash.com/photo-1724093121168-c4ff94115732?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMHx8fGVufDB8fHx8fA%3D%3D",
+    avatar:
+      "https://images.unsplash.com/photo-1723737347273-5ae32dcdb5d3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzOXx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
